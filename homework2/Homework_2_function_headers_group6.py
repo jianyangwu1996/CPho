10653dcf22e127260024c91ed7f9ada63ff7fec7
jianyangwu1996 <w751782826@gmail.com>
2023-06-07 06:31:44 +0200

    solution and test of homework2

--- homework2/Homework_2_function_headers_group6.py
+++ homework2/Homework_2_function_headers_group6.py
@@ -65,13 +65,13 @@ def gauss(xa, Nx, w):
             Generated coordinate vector
     '''
 
-    x = np.linspace(-xa // 2, xa // 2, Nx)
+    x = np.linspace(-xa / 2, xa / 2, Nx)
     v = np.exp(-x ** 2 / w ** 2)
 
     return v, x
 
 
-def beamprop_CN(v_in, lam, dx, n, nd, z_end, dz, output_step):
+def beamprop_CN(v_in, lam, dx, n, nd, z_end, dz, output_step=1):
     '''Propagates an initial field over a given distance based on the
     solution of the paraxial wave equation in an inhomogeneous
     refractive index distribution using the explicit-implicit
@@ -104,25 +104,25 @@ def beamprop_CN(v_in, lam, dx, n, nd, z_end, dz, output_step):
             z-coordinates of field output
     '''
 
-    dz = dz * output_step
-    z = np.arange(0, z_end + 1e-6, dz)
+    size = dz * output_step
+    z = np.arange(0, z_end, size)
 
     N = len(v_in)
     k = 2 * np.pi / lam * n
     k_mean = 2 * np.pi / lam * nd
     W = (k ** 2 - k_mean ** 2) / (2 * k_mean)
-    sec = 1j / (2 * k_mean * dx ** 2) * np.ones(N)
+    sec = (1j / (2 * k_mean * dx ** 2)) * np.ones(N)
     main = -2 * sec + 1j * W
     data = np.array([sec, main, sec])
     offsets = np.array([-1, 0, 1])
     L = sps.dia_array((data, offsets), shape=(N, N)).tocsc()
     I = sps.eye(N)
-    A = I - 0.5 * dz * L
-    B = I + 0.5 * dz * L
+    A = I - 0.5 * size * L
+    B = I + 0.5 * size * L
 
-    v = np.zeros((N, len(z)+1)).astype('complex')
+    v = np.zeros((N, len(z))).astype('complex')
     v[:, 0] = v_in
-    for i in range(1, len(z) + 1):
-        v[:, i] = spsolve(A, B @ v[:, i - 1])
+    for i in range(1, len(z)):
+        v[:, i] = spsolve(A, B @ v[:, i-1])
 
     return v, z
