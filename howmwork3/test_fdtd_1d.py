acd246b87dae213ca18feb90788184f697869501
jianyangwu1996 <w751782826@gmail.com>
2023-06-09 10:16:00 +0200

    task of homework3

---
+++ howmwork3/test_fdtd_1d.py
@@ -0,0 +1,61 @@
+'''Test script for Homework 3, Computational Photonics, SS 2020:  FDTD method.
+'''
+
+
+import numpy as np
+from function_headers_fdtd import fdtd_1d, Fdtd1DAnimation
+from matplotlib import pyplot as plt
+
+# dark bluered colormap, registers automatically with matplotlib on import
+import bluered_dark
+
+plt.rcParams.update({
+        'figure.figsize': (12/2.54, 9/2.54),
+        'figure.subplot.bottom': 0.15,
+        'figure.subplot.left': 0.165,
+        'figure.subplot.right': 0.90,
+        'figure.subplot.top': 0.9,
+        'axes.grid': False,
+        'image.cmap': 'bluered_dark',
+})
+
+plt.close('all')
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# constants
+c = 2.99792458e8 # speed of light [m/s]
+mu0 = 4*np.pi*1e-7 # vacuum permeability [Vs/(Am)]
+eps0 = 1/(mu0*c**2) # vacuum permittivity [As/(Vm)]
+Z0 = np.sqrt(mu0/eps0) # vacuum impedance [Ohm]
+
+# geometry parameters
+x_span = 18e-6 # width of computatinal domain [m]
+n1 = 1 # refractive index in front of interface
+n2 = 2 # refractive index behind interface
+x_interface = x_span/4 #postion of dielectric interface
+
+# simulation parameters
+dx = 15e-9 # grid spacing [m]
+time_span = 60e-15 # duration of simulation [s]
+
+Nx = int(round(x_span/dx)) + 1 # number of grid points
+
+# source parameters
+source_frequency = 500e12 # [Hz]
+source_position = 0 # [m]
+source_pulse_length = 1e-15 # [s]
+
+# %% create permittivity distribution and run simulation %%%%%%%%%%%%%%%%%%%%%%
+
+# please add your code here
+
+# %% make video %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+fps = 25
+step = t[-1]/fps/30
+ani = Fdtd1DAnimation(x, t, Ez, Hy, x_interface=x_interface,
+                       step=step, fps=fps)
+plt.show()
+
+# %% create representative figures of the results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# please add your code here
---
+++ howmwork3/test_fdtd_3d.py
@@ -0,0 +1,98 @@
+'''Test script for Homework 3, Computational Photonics, SS 2020:  FDTD method.
+'''
+
+
+import numpy as np
+from function_headers_fdtd import fdtd_3d, Fdtd3DAnimation
+from matplotlib import pyplot as plt
+
+# dark bluered colormap, registers automatically with matplotlib on import
+import bluered_dark
+
+
+plt.rcParams.update({
+        'figure.figsize': (12/2.54, 9/2.54),
+        'figure.subplot.bottom': 0.15,
+        'figure.subplot.left': 0.165,
+        'figure.subplot.right': 0.90,
+        'figure.subplot.top': 0.9,
+        'axes.grid': False,
+        'image.cmap': 'bluered_dark',
+})
+
+plt.close('all')
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# constants
+c = 2.99792458e8 # speed of light [m/s]
+mu0 = 4*np.pi*1e-7 # vacuum permeability [Vs/(Am)]
+eps0 = 1/(mu0*c**2) # vacuum permittivity [As/(Vm)]
+Z0 = np.sqrt(mu0/eps0) # vacuum impedance [Ohm]
+
+# simulation parameters
+Nx = 199 # number of grid points in x-direction
+Ny = 201 # number of grid points in y-direction
+Nz = 5   # number of grid points in z-direction
+dr = 30e-9 # grid spacing in [m]
+time_span = 10e-15 # duration of simulation [s]
+
+# x coordinates
+x = np.arange(-int(np.ceil((Nx-1)/2)), int(np.floor((Nx-1)/2)) + 1)*dr
+# y coordinates
+y = np.arange(-int(np.ceil((Ny-1)/2)), int(np.floor((Ny-1)/2)) + 1)*dr
+
+# source parameters
+freq = 500e12 # pulse [Hz]
+tau = 1e-15 # pulse width [s]
+source_width = 2 # width of Gaussian current dist. [grid points]
+
+# grid midpoints
+midx = int(np.ceil((Nx-1)/2))
+midy = int(np.ceil((Ny-1)/2))
+midz = int(np.ceil((Nz-1)/2))
+
+
+# %% create relative permittivity distribution %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# eps_rel = ...
+
+# %% current distributions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# jx = jy = np.zeros(...) 
+# jz : Gaussion distribution in the xy-plane with a width of 2 grid points, 
+# constant along z
+
+# jx = ...
+# jy = ...
+# jz = ...
+
+# output parameters
+z_ind = midz # z-index of field output
+output_step = 4 # time steps between field output
+
+#%% run simulations %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# please add your code here
+
+#%% movie of Hx %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+F = Hx*Z0*1e6
+titlestr = 'x-Component of Magnetic Field'
+cb_label = '$\\Re\\{Z_0H_x\\}$ [µV/m]'
+rel_color_range = 1/3
+fps = 10
+
+ani = Fdtd3DAnimation(x, y, t, F, titlestr, cb_label, rel_color_range, fps)
+plt.show()
+
+#%% movie of Ez %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+F = Ez*1e6
+titlestr = 'z-Component of Electric Field'
+cb_label = '$\\Re\\{E_z\\}$ [µV/m]'
+rel_color_range = 1/3
+fps = 10
+
+ani = Fdtd3DAnimation(x, y, t, F, titlestr, cb_label, rel_color_range, fps)
+plt.show()
+
+# %% create representative figures of the results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+# please add your code here
